39321b59976d42087aea03570e14dc95959ec38d
Michael Adams <lanternbugs@gmail.com>
2022-02-05 16:49:18 -0800

    First implementation of mars room. Hard coded to just show mars reading but will show mars and Venus

--- AstrologyApp/Views/MarsRoom.swift
+++ AstrologyApp/Views/MarsRoom.swift
@@ -7,9 +7,49 @@
 
 import SwiftUI
 
-struct MarsRoom: View {
+struct MarsRoom: View, AstrobotReadingInterface {
+    
+    @State var birthdate = Date(timeIntervalSince1970: 0)
+    @State var marsData = [ReadingEntry(text: "Pick a date and select go for an Evengeline Adams Mars Reading", id: 0)]
+    @State var venusData = [ReadingEntry(text: "Pick a date and select go for an Evengeline Adams Venus Reading", id: 0)]
+    @State var planetChoice: Planets = .Mars
+    var activeReading: [ReadingEntry] {
+        switch(planetChoice) {
+        case .Mars:
+            return marsData
+        case .Venus:
+            return venusData
+        default:
+            return [ReadingEntry(text: "Invalid State", id: 0)]
+        }
+    }
     var body: some View {
-        Text("Mars Room")
+        
+        List {
+            HStack {
+                DatePicker(
+                  "Birthdate",
+                  selection: $birthdate,
+                  displayedComponents: .date
+                ).datePickerStyle(DefaultDatePickerStyle())
+                Button(action: pickedDate) {
+                    Text("Go").font(.title)
+                }
+            }
+            ForEach(activeReading, id: \.id) {
+                paragraph in
+                Text("\(paragraph.text)")
+                
+            }
+            
+        }
+    }
+}
+
+extension MarsRoom {
+    func pickedDate()  {
+        marsData = getPlanet(type: .Mars, time: birthdate.getAstroTime()).getReading()
+        venusData = getPlanet(type: .Venus, time: birthdate.getAstroTime()).getReading()
     }
 }
 
