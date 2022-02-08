8dcfaea47d8acbec1d17059869396c26f1681e4c
Michael Adams <lanternbugs@gmail.com>
2022-02-07 19:26:51 -0800

    Updated cities view to be part of a navigation view and have cites list be searchable

--- AstrologyApp/Views/CitiesView.swift
+++ AstrologyApp/Views/CitiesView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct CitiesView: View {
     @ObservedObject var citiesData = BirthDateManager()
-    var body: some View {
-        List(citiesData.cityInfo?.cities ?? [],id: \.id) {
-            city in
-            HStack {
-                Spacer()
-                Text("\(city.city)").frame(maxWidth: .infinity, alignment: .leading)
-                Spacer()
-                Text("\(city.country)").frame(maxWidth: .infinity, alignment: .leading)
-                Spacer()
+    @State private var city = ""
+    var displayCities: [City] {
+        if let  cities = citiesData.cityInfo?.cities
+        {
+            let data = cities.filter {
+                if cities.isEmpty {
+                    return true
+                }
+                return $0.city.lowercased().hasPrefix(city.lowercased())
             }
-            
+            return data
         }
+        return [City]()
+         
+    }
+    var body: some View {
+        VStack {
+            // text could be in search bar with searchable but this is to work in ios 14 too
+            TextField("City", text: $city)
+            List(displayCities, id: \.id) {
+                city in
+                HStack {
+                    Spacer()
+                    Text("\(city.city)").frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    Text("\(city.country)").frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                }
+            }
+        }.navigationTitle("Cities")
+        
     }
 }
 
