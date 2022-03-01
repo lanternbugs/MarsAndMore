947d7792e6766ae512624dc24abc4c3f063ef8f8
Michael Adams <lanternbugs@gmail.com>
2022-02-28 18:09:37 -0800

    Removed id from CoreData entity since name is unique always

--- AstroApp/BirthDataManager/extensions/BirthDataManager+Extensions.swift
+++ AstroApp/BirthDataManager/extensions/BirthDataManager+Extensions.swift
@@ -22,7 +22,6 @@ extension BirthDataManager {
         }
         if let entity =  NSEntityDescription.entity(forEntityName: "BirthDates", in: context) {
             let entry = BirthDates(entity: entity, insertInto: context)
-            entry.id = Int32(data.id)
             entry.name = data.name
             entry.year = data.birthDate.year
             entry.month = data.birthDate.month
@@ -49,7 +48,13 @@ extension BirthDataManager {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "BirthDates")
         do {
             if let birthDates =  try context.fetch(fetchRequest) as? [BirthDates] {
-                for entry in birthDates {
+                let dates = birthDates.sorted {
+                    if let name1 = $0.name, let name2 = $1.name {
+                        return name2 > name1
+                    }
+                    return true
+                }
+                for entry in dates {
                     var location: LocationData?
                     if let lat = entry.latitude, let long = entry.longitude {
                        location = LocationData(latitude: lat, longitude: long)
@@ -58,7 +63,7 @@ extension BirthDataManager {
                     guard let name = entry.name else {
                         continue
                     }
-                    let data = BirthData(name: name, birthDate: date, birthTime: entry.time , location: location, id: Int(entry.id))
+                    let data = BirthData(name: name, birthDate: date, birthTime: entry.time , location: location, id: self.birthDates.count)
                     self.birthDates.append(data)
                     
                 }
