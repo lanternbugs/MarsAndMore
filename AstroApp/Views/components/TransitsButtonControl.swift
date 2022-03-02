84f6f8f11655607e99ad05b62a6d75c220815a70
Michael Adams <lanternbugs@gmail.com>
2022-03-02 14:07:51 -0800

    First layout for transitsButtonControl

--- AstroApp/Views/components/TransitsButtonControl.swift
+++ AstroApp/Views/components/TransitsButtonControl.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct TransitsButtonControl: View, AstrobotInterface {
+    
     @EnvironmentObject private var manager: BirthDataManager
+    @State private var birthdate: Date = Date(timeIntervalSince1970: 0)
     var body: some View {
-        HStack {
-            Spacer()
-            Button(action: transits) {
-                Text("Transits")
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: transits) {
+                    Text("Transits")
+                }
+                Spacer()
+                if let name = manager.getCurrentName() {
+                    Text("\(name)")
+                    Spacer()
+                }
             }
-            Spacer()
+            DatePicker(
+              "On",
+              selection: $birthdate,
+              displayedComponents: [.date, .hourAndMinute]
+            ).datePickerStyle(DefaultDatePickerStyle())
         }
+        
     }
 }
 
