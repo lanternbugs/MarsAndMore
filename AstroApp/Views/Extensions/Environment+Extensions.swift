93fb45171ff00de4d65d1da68f874788f9770430
Michael Adams <lanternbugs@gmail.com>
2022-03-25 14:56:27 -0700

    Added copyright and license to files that were missing it

--- AstroApp/Views/Extensions/Environment+Extensions.swift
+++ AstroApp/Views/Extensions/Environment+Extensions.swift
@@ -1,9 +1,15 @@
-//
-//  Environment+Extensions.swift
-//  MarsAndMore
-//
-//  Created by Michael Adams on 2/27/22.
-//
+/*
+*  Copyright (C) 2022 Michael R Adams.
+*  All rights reserved.
+*
+* This program can be redistributed and/or modified under
+* the terms of the GNU General Public License; either
+* version 2 of the License, or (at your option) any later version.
+*
+*  This code is distributed in the hope that it will
+*  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 
 import SwiftUI
 
