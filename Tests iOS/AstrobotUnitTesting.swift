9fa0284d733ad72eb9a0633a7dd36e8aed57351e
Michael Adams <lanternbugs@gmail.com>
2022-07-12 19:17:41 -0700

    Implemented first of month test case

--- Tests iOS/AstrobotUnitTesting.swift
+++ Tests iOS/AstrobotUnitTesting.swift
@@ -60,9 +60,40 @@ class AstrobotUnitTesting: XCTestCase {
         }
     }
     
-    func testSunSignIsCorrestForFirstOfMonth() throws
+    func testSunSignIsCorrectForFirstOfMonth() throws
     {
+        let months: [Int: Signs] = [1: .Capricorn, 2: .Aquarius, 3: .Pisces, 4: .Aries, 5: .Taurus, 6: .Gemini, 7: .Cancer, 8: .Leo, 9: .Virgo, 10: .Libra, 11: .Scorpio, 12: .Sagitarius ]
+        let year = Int.random(in: 1800...2100)
+        let day = 1
+        for month in 1...12
+        {
+            let date = getDate(y: year, m: month, d: day)
+            guard let date = date else {
+                XCTAssertNotNil(date)
+                return;
+            }
+            let row = sut.getPlanets(time: date.getAstroTime(), location: nil)
+            let planet = row.planets.first { $0.planet == .Sun}
+            guard let planet = planet as? PlanetCell else {
+                XCTAssertNotNil(planet)
+                return
+            }
+            XCTAssertEqual(planet.sign, months[month])
+        }
+    }
+    
+    func getDate(y: Int, m: Int, d: Int)->Date?
+    {
+        let calendar = Calendar.current
 
+            var dateComponents: DateComponents? = calendar.dateComponents([.hour, .minute, .second], from: Date())
+
+            dateComponents?.day = d
+            dateComponents?.month = m
+            dateComponents?.year = y
+
+            let date: Date? = calendar.date(from: dateComponents!)
+            return date
     }
 
 }
