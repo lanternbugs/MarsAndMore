5aff40ee3564de43137859ad4a228537d50931e8
Michael Adams <lanternbugs@gmail.com>
2022-07-22 12:18:16 -0700

    Updated model for mars rover photos parsing

--- AstroApp/Space/Models/SpaceModels.swift
+++ AstroApp/Space/Models/SpaceModels.swift
@@ -23,3 +23,61 @@ struct PictureOfDay: Codable
     let title: String
     let url: URL
 }
+
+struct RoverManifest: Codable
+{
+    let photo_manifest: ManifestData
+}
+struct ManifestData: Codable
+{
+    let name: String
+    let landing_date: String
+    let launch_date: String
+    let status: String
+    let max_sol: Int
+    let max_date: String
+    let total_photos: Int
+    let photos: [PhotoData]
+}
+
+struct PhotoData: Codable
+{
+    let sol: Int
+    let earth_date: String
+    let total_photos: Int
+    let cameras:Array<String>
+}
+struct RoverPhotos: Codable
+{
+    var photos: [RoverPhoto]
+}
+
+struct RoverPhoto: Codable
+{
+    var id: Int
+    var sol: Int
+    var camera: RoverCamera
+    var img_src: URL
+    var earth_date: String
+    var rover: Rover
+    
+    
+}
+
+struct RoverCamera: Codable
+{
+    var id: Int
+    var name: String
+    var rover_id: Int
+    var full_name: String
+}
+
+struct Rover: Codable
+{
+    var id: Int
+    var name: String
+    var landing_date: String
+    var launch_date: String
+    var status: String
+}
+
