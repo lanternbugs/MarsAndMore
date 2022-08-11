36700d48764e004c0733eb744f8eb415e98ca50f
Michael Adams <lanternbugs@gmail.com>
2022-08-11 14:34:34 -0700

    Made text selection of object id possible so they can be copied later

--- AstroApp/Views/ArtView.swift
+++ AstroApp/Views/ArtView.swift
@@ -15,7 +15,21 @@ struct ArtView: View {
                 ForEach(artDataManager.artData, id: \.id) {
                     image in
                     Text(image.name)
-                    Text(String(image.objectId))
+#if os(macOS)
+            if #available(macOS 12.0, *) {
+                Text(String(image.objectId)).textSelection(.enabled)
+            }
+            else {
+                Text(String(image.objectId))
+                }
+#else
+            if #available(iOS 15.0, *) {
+                Text(String(image.objectId)).textSelection(.enabled)
+            }
+            else {
+                Text(String(image.objectId))
+                }
+#endif
                     DelayedImageView(url: image.url, key: nil)
                 }
             }
