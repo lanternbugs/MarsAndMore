185ac2683b515aa20a139d6e1e7317133cef172b
Michael Adams <lanternbugs@gmail.com>
2022-08-15 18:58:15 -0700

    Now fetching and displaying both mars and Venus themed art on art tab

--- AstroApp/Views/ArtView.swift
+++ AstroApp/Views/ArtView.swift
@@ -9,57 +9,41 @@ import SwiftUI
 
 struct ArtView: View {
     @EnvironmentObject private var artDataManager: ArtDataManager
+    @State private var planetChoice: Planets = Planets.Mars
     var body: some View {
         ScrollView {
+            
+            
             LazyVStack {
-                ForEach(artDataManager.artData, id: \.id) {
-                    image in
-#if os(macOS)
-            if #available(macOS 12.0, *) {
-                Text(image.name).textSelection(.enabled)
-                if let artistName = image.artistDisplayName, artistName.count > 0 {
-                    Text("by \(artistName)").textSelection(.enabled)
+                HStack {
+                    Spacer()
+                    Text("Mars and Venus themed Art")
+                    Spacer()
                 }
-                if let objDate = image.objectDate {
-                    Text(objDate).textSelection(.enabled)
+                HStack {
+                    Spacer()
+                    Text("Images update daily")
+                    Spacer()
                 }
-                Text(String(image.objectId)).textSelection(.enabled)
+                Picker(selection: $planetChoice, label: Text("Choice")) {
+                    Text("Mars").tag(Planets.Mars)
+                    Text("Venus").tag(Planets.Venus)
+                }.background(Color.white).pickerStyle(SegmentedPickerStyle())
+                switch planetChoice {
+                case .Mars:
+                    ForEach(artDataManager.marsArtData, id: \.id) {
+                        image in
+                        ArtDisplayView(image: image)
+                    }
+                case .Venus:
+                    ForEach(artDataManager.venusArtData, id: \.id) {
+                        image in
+                        ArtDisplayView(image: image)
+                    }
+                default:
+                    EmptyView()
+                }
                 
-            }
-            else {
-                Text(image.name)
-                if let artistName = image.artistDisplayName, artistName.count > 0 {
-                    Text("by \(artistName)")
-                }
-                if let objDate = image.objectDate {
-                    Text(objDate)
-                }
-                Text(String(image.objectId))
-                }
-#else
-            if #available(iOS 15.0, *) {
-                Text(image.name).textSelection(.enabled)
-                if let artistName = image.artistDisplayName, artistName.count > 0 {
-                    Text("by \(artistName)").textSelection(.enabled)
-                }
-                if let objDate = image.objectDate {
-                    Text(objDate).textSelection(.enabled)
-                }
-                Text(String(image.objectId)).textSelection(.enabled)
-            }
-            else {
-                Text(image.name)
-                if let artistName = image.artistDisplayName, artistName.count > 0 {
-                    Text("by \(artistName)")
-                }
-                if let objDate = image.objectDate {
-                    Text(objDate)
-                }
-                Text(String(image.objectId))
-                }
-#endif
-                    DelayedImageView(url: image.url, key: nil)
-                }
             }
         }
     }
