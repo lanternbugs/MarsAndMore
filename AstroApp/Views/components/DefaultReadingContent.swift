58f185a8b1aedc253a76a9e89e5c23339a02e327
Michael Adams <lanternbugs@gmail.com>
2023-11-05 12:16:36 -0800

    edited default reading message and upped version for release to 1.50

--- AstroApp/Views/components/DefaultReadingContent.swift
+++ AstroApp/Views/components/DefaultReadingContent.swift
@@ -14,7 +14,7 @@
 import SwiftUI
 
 struct DefaultReadingContent: View {
-    let defaultReadingMessage = "Pick a date and select go for an Evengeline Adams Mars and Venus Reading"
+    let defaultReadingMessage = "Pick a date and select go for an Evengeline Adams(ghostwriter Crowley) Sun, Moon, Mercury, Venus and Mars Reading"
     let defaultReadingSecondaryMessage = "When reading the planet/sign interpetations, be aware as stated by Adams, \"Of course, the usual modifications caused by aspects are always to be considered.\"."
     let defaultReadingThirdMessage =  "These interpetations were written in the 1930s. An older mindset was involved, though progressive for its time using Astrology"
     var body: some View {
