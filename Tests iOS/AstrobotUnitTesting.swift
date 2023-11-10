1f9404bed73c8426ae9a6f06314005c4b0360d69
Michael Adams <lanternbugs@gmail.com>
2023-11-09 17:49:48 -0800

    fixed unit tests for new calculation struct

--- Tests iOS/AstrobotUnitTesting.swift
+++ Tests iOS/AstrobotUnitTesting.swift
@@ -1,9 +1,15 @@
-//
-//  AstrobotUnitTesting.swift
-//  Tests iOS
-//
-//  Created by Michael Adams on 7/9/22.
-//
+/*
+*  Copyright (C) 2022-2023 Michael R Adams.
+*  All rights reserved.
+*
+* This program can be redistributed and/or modified under
+* the terms of the GNU General Public License; either
+* version 2 of the License, or (at your option) any later version.
+*
+*  This code is distributed in the hope that it will
+*  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 
 import XCTest
 @testable import MarsAndMore
@@ -25,13 +31,13 @@ class AstrobotUnitTesting: XCTestCase {
 
     func testPlanetsArrayNotEmpty() throws
     {
-        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil)
+        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil, calculationSettings: CalculationSettings())
         XCTAssertGreaterThan(row.planets.count, 0)
     }
     
     func testDegreeStringsNotEmpty() throws
     {
-        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil)
+        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil, calculationSettings: CalculationSettings())
         for planet in row.planets {
             XCTAssertGreaterThan(planet.degree.count, 0)
         }
@@ -39,17 +45,20 @@ class AstrobotUnitTesting: XCTestCase {
 
     func testNoPlanetsDuplicated() throws
     {
-        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil)
+        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil, calculationSettings: CalculationSettings())
         var planetSet = Set<Planets>()
-        for planet in row.planets {
-            XCTAssertTrue(!planetSet.contains(planet.planet))
-            planetSet.insert(planet.planet)
+        if let planets  =  row.planets as? [PlanetCell] {
+            for planet in planets {
+                XCTAssertTrue(!planetSet.contains(planet.planet))
+                planetSet.insert(planet.planet)
+            }
         }
+        
     }
         
     func testNoSignNone() throws
     {
-        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil)
+        let row = sut.getPlanets(time: Date().getAstroTime(), location: nil, calculationSettings: CalculationSettings())
         for planet in row.planets {
             let planetCell = planet as? PlanetCell
             XCTAssertNotNil(planetCell)
@@ -72,25 +81,29 @@ class AstrobotUnitTesting: XCTestCase {
                 XCTAssertNotNil(date)
                 return;
             }
-            let row = sut.getPlanets(time: date.getAstroTime(), location: nil)
-            let planet = row.planets.first { $0.planet == .Sun}
-            guard let planet = planet as? PlanetCell else {
-                XCTAssertNotNil(planet)
-                return
+            let row = sut.getPlanets(time: date.getAstroTime(), location: nil, calculationSettings: CalculationSettings())
+            if let planets  =  row.planets as? [PlanetCell] {
+                
+                let planet = planets.first { $0.planet == .Sun}
+                guard let planet = planet else {
+                    XCTAssertNotNil(planet)
+                    return
+                }
+                XCTAssertEqual(planet.sign, months[month])
             }
-            XCTAssertEqual(planet.sign, months[month])
+            
         }
     }
     
     func testAspectsArrayNotEmpty() throws
     {
-        let row = sut.getAspects(time: Date().getAstroTime(), with: nil, and: nil)
+        let row = sut.getAspects(time: Date().getAstroTime(), with: nil, and: nil, calculationSettings: CalculationSettings())
         XCTAssertGreaterThan(row.planets.count, 0)
     }
     
     func testAspectsAspectDifferentPlanet() throws
     {
-        let row = sut.getAspects(time: Date().getAstroTime(), with: nil, and: nil)
+        let row = sut.getAspects(time: Date().getAstroTime(), with: nil, and: nil, calculationSettings: CalculationSettings())
         for planet in row.planets {
             let planetCell = planet as? TransitCell
             XCTAssertNotNil(planetCell)
