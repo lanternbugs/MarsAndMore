aab7db202bc240f8e4832f09f5a5de3a8b11dbb2
Michael Adams <lanternbugs@gmail.com>
2023-11-09 17:51:04 -0800

    added licence to files missing it

--- Tests iOS/CitiesInfoUnitTesting.swift
+++ Tests iOS/CitiesInfoUnitTesting.swift
@@ -1,9 +1,16 @@
-//
-//  CitiesInfoUnitTesting.swift
-//  Tests iOS
-//
-//  Created by Michael Adams on 7/14/22.
-//
+/*
+*  Copyright (C) 2022-2023 Michael R Adams.
+*  All rights reserved.
+*
+* This program can be redistributed and/or modified under
+* the terms of the GNU General Public License; either
+* version 2 of the License, or (at your option) any later version.
+*
+*  This code is distributed in the hope that it will
+*  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 
 import XCTest
 @testable import MarsAndMore
