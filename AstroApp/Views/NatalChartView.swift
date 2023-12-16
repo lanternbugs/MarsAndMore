dd05e04a4d81d2c0c4d669ca21a2fd89da2ca44d
Michael Adams <lanternbugs@gmail.com>
2023-12-15 18:11:56 -0800

    added licence to new files that were missing it

--- AstroApp/Views/NatalChartView.swift
+++ AstroApp/Views/NatalChartView.swift
@@ -1,7 +1,15 @@
-//
-//  NatalChartView.swift
-//  MarsAndMore
-//
+/*
+*  Copyright (C) 2022-23 Michael R Adams.
+*  All rights reserved.
+*
+* This program can be redistributed and/or modified under
+* the terms of the GNU General Public License; either
+* version 2 of the License, or (at your option) any later version.
+*
+*  This code is distributed in the hope that it will
+*  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 //  Created by Michael Adams on 11/30/23.
 //
 
