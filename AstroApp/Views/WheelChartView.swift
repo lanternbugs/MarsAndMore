d4aee62d6d927e56c85536a65f136a3fa79ef567
Michael Adams <lanternbugs@gmail.com>
2024-04-09 09:42:02 -0700

    refactored wheel chart data at bottom to a component view

--- AstroApp/Views/WheelChartView.swift
+++ AstroApp/Views/WheelChartView.swift
@@ -62,82 +62,13 @@ struct WheelChartView: View {
                 NatalViewRepresentable(model: viewModel).frame(width: getScreenWidth(), height: getScreenWidth())
 #endif
             }
-                if viewModel.chart == .Natal {
-                    HStack {
-                        Spacer()
-                        Text("Planets").font(.title2)
-                        Spacer()
-                    }
-                    ForEach(viewModel.planetData.sorted(by: { if $0.planet == .MC && $1.planet == .Ascendent {
-                        return false
-                    } else if $0.planet == .Ascendent && $1.planet == .MC {
-                        return true
-                    }
-                       return $0.planet.rawValue < $1.planet.rawValue }), id: \.id) {
-                        data in
-                        HStack {
-                            if manager.bodiesToShow.contains(data.planet) {
-                                Text(getPlanetRow(data))
-                                Spacer()
-                            }
-                            
-                        }
-                    }
-                }
-                    if !viewModel.aspectsData.isEmpty {
-                        HStack {
-                            Spacer()
-                            if viewModel.chart == .Synastry || viewModel.chart == .Natal {
-                                Text("Aspects").font(.title2)
-                            } else {
-                                Text("Transits").font(.title2)
-                            }
-                            
-                            Spacer()
-                        }
-                        ForEach(viewModel.aspectsData.sorted(by: {
-                            if $0.planet2 == .MC && $1.planet2 == .Ascendent  {
-                                return false
-                            }
-                            if $0.planet2 == .Ascendent && $1.planet2 == .MC  {
-                                return true
-                            }
-                            return $0.planet2.rawValue < $1.planet2.rawValue }), id: \.id) {
-                            data in
-                            HStack {
-                                if data.aspect.isMajor() && manager.bodiesToShow.contains(data.planet) && manager.bodiesToShow.contains(data.planet2){
-                                    Text(getAspectRow(data))
-                                    Spacer()
-                                }
-                                
-                            }
-                        }
-                    }
-               
-                
-
-                
+             WheelChartData(viewModel: viewModel) 
             }
         }
     }
 }
 
 extension WheelChartView {
-    func getPlanetRow(_ data: PlanetCell) -> String {
-        var text =  data.planet.getName().uppercased() + " " + data.sign.getName() + " " + data.degree
-        if data.retrograde {
-            text = text + " " + "R"
-        }
-        
-        return text
-    }
-    func getAspectRow(_ data: TransitCell) -> String {
-        if viewModel.chart == .Transit {
-            return data.planet.getName() + " " + data.aspect.getName() + " " + data.planet2.getName() + " " + data.degree
-        }
-        return data.planet2.getName() + " " + data.aspect.getName() + " " + data.planet.getName() + " " + data.degree
-    }
-    
     func getScreenWidth()->Double
     {
 #if os(macOS)
--- AstroApp/Views/components/WheelChartData.swift
+++ AstroApp/Views/components/WheelChartData.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct WheelChartData: View {
+    let viewModel: ChartViewModel
+    @EnvironmentObject var manager:BirthDataManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if viewModel.chart == .Natal {
+                HStack {
+                    Spacer()
+                    Text("Planets").font(.title2)
+                    Spacer()
+                }
+                ForEach(viewModel.planetData.sorted(by: { if $0.planet == .MC && $1.planet == .Ascendent {
+                    return false
+                } else if $0.planet == .Ascendent && $1.planet == .MC {
+                    return true
+                }
+                   return $0.planet.rawValue < $1.planet.rawValue }), id: \.id) {
+                    data in
+                    HStack {
+                        if manager.bodiesToShow.contains(data.planet) {
+                            Text(getPlanetRow(data))
+                            Spacer()
+                        }
+                        
+                    }
+                }
+            }
+                if !viewModel.aspectsData.isEmpty {
+                    HStack {
+                        Spacer()
+                        if viewModel.chart == .Synastry || viewModel.chart == .Natal {
+                            Text("Aspects").font(.title2)
+                        } else {
+                            Text("Transits").font(.title2)
+                        }
+                        
+                        Spacer()
+                    }
+                    ForEach(viewModel.aspectsData.sorted(by: {
+                        if $0.planet2 == .MC && $1.planet2 == .Ascendent  {
+                            return false
+                        }
+                        if $0.planet2 == .Ascendent && $1.planet2 == .MC  {
+                            return true
+                        }
+                        return $0.planet2.rawValue < $1.planet2.rawValue }), id: \.id) {
+                        data in
+                        HStack {
+                            if data.aspect.isMajor() && manager.bodiesToShow.contains(data.planet) && manager.bodiesToShow.contains(data.planet2){
+                                Text(getAspectRow(data))
+                                Spacer()
+                            }
+                            
+                        }
+                    }
+                }
+           
+        }
+    }
+}
+
+extension WheelChartData {
+    func getPlanetRow(_ data: PlanetCell) -> String {
+        var text =  data.planet.getName().uppercased() + " " + data.sign.getName() + " " + data.degree
+        if data.retrograde {
+            text = text + " " + "R"
+        }
+        
+        return text
+    }
+    func getAspectRow(_ data: TransitCell) -> String {
+        if viewModel.chart == .Transit {
+            return data.planet.getName() + " " + data.aspect.getName() + " " + data.planet2.getName() + " " + data.degree
+        }
+        return data.planet2.getName() + " " + data.aspect.getName() + " " + data.planet.getName() + " " + data.degree
     }
 }
 
 #Preview {
-    WheelChartData()
+    WheelChartData(viewModel: ChartViewModel(chartName: "mike", chartType: .Natal))
 }
