f76a608015df7e01154fb6df39e212b3f8e9f334
Michael Adams <lanternbugs@gmail.com>
2024-07-28 15:17:18 -0700

    made planet signs symbols larger on mac and ipad in resource view

--- AstroApp/Views/ResourcesView.swift
+++ AstroApp/Views/ResourcesView.swift
@@ -19,29 +19,35 @@ struct ResourcesView: View {
     let cafeAstrology = "https://cafeastrology.com/siteindex.html"
     let columns: [GridItem] =
                  Array(repeating: .init(.flexible()), count: 2)
+#if os(iOS)
+    let symbolFontSize = UIDevice.current.userInterfaceIdiom == .pad ? 32.0 : 20.0
+#else
+    let symbolFontSize = 32.0
+#endif
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
                 Text("For those new to Astrological Charts and wondering what does it mean, this page hopes to offer a few resources and tips").font(.headline)
-                Text("First enter a name with a birth date in chart room by tapping + button to begin.")
-                Text("Planets button used with that name selected gives the signs the planets were in at birth or what is called the Natal Chart.").padding([.bottom])
+                Text("First enter a name with a birth date in chart room by tapping + button to begin.").font(.body)
+                Text("Planets button used with that name selected gives the signs the planets were in at birth or what is called the Natal Chart.").font(.body).padding([.bottom])
                 Text("Signs are shortened to first three letters so Sco is Scorpio. If it says Jupiter is in Scorpio then users can Google natal jupiter scorpio").font(.headline)
-                Text("Also two sites that come up near top of Google I've noticed for general info on all aspects or transits are astrologyking.com and cafeastrology.com. MarsAndMore has no affiliation with them.")
-                Text("Users can find text descriptions of Planets, Aspects and Transits that are shown in Chart Room at these three links below, versus just searching in a search engine.")
+                Text("Also two sites that come up near top of Google I've noticed for general info on all aspects or transits are astrologyking.com and cafeastrology.com. MarsAndMore has no affiliation with them.").font(.body)
+                Text("Users can find text descriptions of Planets, Aspects and Transits that are shown in Chart Room at these three links below, versus just searching in a search engine.").font(.body)
                 Button(action: {
                     self.openLink(link: cafeAstrology)
                 }) {
-                    Text(cafeAstrology).foregroundColor(Color.blue)
+                    Text(cafeAstrology).font(.body).foregroundColor(Color.blue)
                 }.padding()
                 Button(action: {
                     self.openLink(link: astrologyKingAspects)
                 }) {
-                    Text(astrologyKingAspects).foregroundColor(Color.blue)
+                    Text(astrologyKingAspects).font(.body).foregroundColor(Color.blue)
                 }.padding()
                 Button(action: {
                     self.openLink(link: astrologyKingTransits)
                 }) {
-                    Text(astrologyKingTransits).foregroundColor(Color.blue)
+                    Text(astrologyKingTransits).font(.body).foregroundColor(Color.blue)
                 }.padding()
             }.padding([.trailing, .leading])
             HStack {
@@ -52,7 +58,7 @@ struct ResourcesView: View {
                 aspect in
                 if aspect.isMajor() {
                     HStack {
-                        Text("\(aspect.getName()) \(Int(aspect.rawValue))")
+                        Text("\(aspect.getName()) \(Int(aspect.rawValue))").font(.body)
                         Spacer()
                     }
                 }
@@ -66,11 +72,12 @@ struct ResourcesView: View {
                 aspect in
                 if !aspect.isMajor() {
                     HStack {
-                        Text("\(aspect.getName()) \(Int(aspect.rawValue))")
+                        Text("\(aspect.getName()) \(Int(aspect.rawValue))").font(.body)
                         Spacer()
                     }
                 }
             }
+
             Text("")
             HStack {
                 Text("Wheel Chart Planet Symbols").font(.title2)
@@ -82,23 +89,23 @@ struct ResourcesView: View {
                     switch(planet) {
                     case .Pholus:
                         HStack {
-                            Text(" \(planet.getName()) - No Symbol")
+                            Text(" \(planet.getName()) - No Symbol").font(.body)
                             Spacer()
                         }
                     case .Ascendent:
                         HStack {
-                            Text(" \(planet.getName()) - Ascendent")
+                            Text(" \(planet.getName()) - Ascendent").font(.body)
                             Spacer()
                         }
                     case .MC:
                         HStack {
-                            Text(" \(planet.getName()) - Medium Coeli")
+                            Text(" \(planet.getName()) - Medium Coeli").font(.body)
                             Spacer()
                         }
                     default:
                         HStack {
-                            Text(" \(planet.getAstroDotCharacter())").font(Font.custom("AstroDotBasic", size: 20))
-                            Text(planet.getName())
+                            Text(" \(planet.getAstroDotCharacter())").font(Font.custom("AstroDotBasic", size: symbolFontSize))
+                            Text(planet.getName()).font(.body)
                             Spacer()
                         }
                     }
@@ -120,8 +127,8 @@ struct ResourcesView: View {
                         }
                     default:
                         HStack {
-                            Text(" \(sign.getAstroDotCharacter())").font(Font.custom("AstroDotBasic", size: 20))
-                            Text(sign.getName())
+                            Text(" \(sign.getAstroDotCharacter())").font(Font.custom("AstroDotBasic", size: symbolFontSize))
+                            Text(sign.getName()).font(.body)
                             Spacer()
                         }
                     }
