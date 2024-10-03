151748958e530d8fa64f83ff380f9bedeec415ed
Michael Adams <lanternbugs@gmail.com>
2024-10-03 13:53:55 -0700

    added new view IndividualCompositeView

---
+++ AstroApp/Views/components/IndividualCompositeDataView.swift
@@ -0,0 +1,18 @@
+//
+//  IndividualCompositeDataView.swift
+//  MarsAndMore
+//
+//  Created by Michael Adams on 10/3/24.
+//
+
+import SwiftUI
+
+struct IndividualCompositeDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndividualCompositeDataView()
+}
