9d857c7a17d56e770103f3d36c01d9d1c0b239cd
Michael Adams <lanternbugs@gmail.com>
2024-10-12 14:50:19 -0700

    start of ui to pick latitude and longitude with pickers

--- AstroApp/ViewModels/EditLocationDataViewModel.swift
+++ AstroApp/ViewModels/EditLocationDataViewModel.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 class EditLocationDataViewModel {
+    var latDegreeChoices = [(Int, Int)]()
+    var latMinuteChoices = [(Int, Int)]()
+    var latSecondChoices = [(Int, Int)]()
+    var longDegreeChoices = [(Int, Int)]()
+    var longMinuteChoices = [(Int, Int)]()
+    var longSecondChoices = [(Int, Int)]()
+    var latitudeDirection = ["N", "S"]
+    var longitudeDirection = ["E", "W"]
+    let latDegreeOffset = 100
+    let latMinuteOffset = 1000
+    let latSecondOffset = 10000
+    let longDegreeOffset = 200
+    let longMinuteOffset = 2000
+    let longSecondOffset = 20000
     
+    init() {
+        populateChoices()
+    }
+    
+    func populateChoices() {
+        for i in 0...59 {
+            latDegreeChoices.append((i, i + latDegreeOffset))
+            latMinuteChoices.append((i, i + latMinuteOffset))
+            latSecondChoices.append((i, i + latSecondOffset))
+            longDegreeChoices.append((i, i + longDegreeOffset))
+            longMinuteChoices.append((i, i + longMinuteOffset))
+            longSecondChoices.append((i, i + longSecondOffset))
+        }
+    }
 }
--- AstroApp/Views/EditLocationDataView.swift
+++ AstroApp/Views/EditLocationDataView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct EditLocationDataView: View {
+    @State var latDegree = 100
+    @State var latMinute = 1000
+    @State var latSecond = 10000
+    @State var longDegree = 200
+    @State var longMinute = 2000
+    @State var longSecond = 20000
+    @State var latitudeDirection = "N"
+    @State var longitudeDirection = "E"
+    
+    let viewModel = EditLocationDataViewModel()
     var body: some View {
         VStack {
-            Text("Edit Location Data")
+            Text("Latitude")
+            Picker(selection: $latitudeDirection, label: Text("Direction")) {
+                ForEach(viewModel.latitudeDirection, id: \.self) { choice in
+                        Text(choice)
+                    }
+            }
+            Picker(selection: $latDegree, label: Text("Degree")) {
+                ForEach(viewModel.latDegreeChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
+            Picker(selection: $latMinute, label: Text("Minute")) {
+                ForEach(viewModel.latMinuteChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
+            Picker(selection: $latSecond, label: Text("Second")) {
+                ForEach(viewModel.latSecondChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
+            
+            Text("Longitude")
+            Picker(selection: $longitudeDirection, label: Text("Direction")) {
+                ForEach(viewModel.longitudeDirection, id: \.self) { choice in
+                        Text(choice)
+                    }
+            }
+            Picker(selection: $longDegree, label: Text("Degree")) {
+                ForEach(viewModel.longDegreeChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
+            Picker(selection: $longMinute, label: Text("Minute")) {
+                ForEach(viewModel.longMinuteChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
+            Picker(selection: $longSecond, label: Text("Second")) {
+                ForEach(viewModel.longSecondChoices, id: \.self.1) {
+                    choice in
+                    Text(String(choice.0))
+                }
+            }
             Spacer()
         }
         
     }
 }
+
 
 #Preview {
     EditLocationDataView()
