c37dd58e0915bd7fd18be3074d252a58aa3aadfe
Michael Adams <lanternbugs@gmail.com>
2024-10-26 09:33:47 -0700

    added symbol key option to wheel chart data when in symbol mode

--- AstroApp/Views/components/WheelChartData.swift
+++ AstroApp/Views/components/WheelChartData.swift
@@ -16,6 +16,7 @@
 import SwiftUI
 
 struct WheelChartData: View {
+    @State var showSymbolKey = false
     let viewModel: ChartViewModel
     @EnvironmentObject var manager:BirthDataManager
     var body: some View {
@@ -24,13 +25,29 @@ struct WheelChartData: View {
                 if manager.chartDataSymbols {
                     Button(action:  { manager.chartDataSymbols = false }) {
                                             Text("Text")
-                                        }
+                    }.padding(.leading)
                                     } else {
                                         Button(action:  { manager.chartDataSymbols = true }) {
                                             Text("Symbols")
-                                        }
+                                        }.padding(.leading)
                                     }
                 Spacer()
+                if manager.chartDataSymbols {
+                    if showSymbolKey {
+                        Button(action:  { showSymbolKey.toggle()
+                            }) {
+                            Text("Hide Key")
+                            }.padding(.trailing)
+                    } else {
+                        Button(action:  { showSymbolKey.toggle()
+                            }) {
+                            Text("Key")
+                            }.padding(.trailing)
+                    }
+                }
+            }
+            if manager.chartDataSymbols && showSymbolKey {
+                AstroSymbolsKey(showAspectsSymbols: true)
             }
             if viewModel.chart == .Natal {
                 WheelChartPlanetListing(viewModel: WheelChartDataViewModel(planets: viewModel.planetData, aspects: viewModel.aspectsData, houses: viewModel.houseData, chart: .Natal, title: "Planets", houseSystem: manager.houseSystem))
