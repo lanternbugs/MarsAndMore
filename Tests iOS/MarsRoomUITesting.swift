b477a6cd681e6197cb99408eb972a2443ec3d7d7
Michael Adams <lanternbugs@gmail.com>
2024-11-30 09:10:35 -0800

    first mars tab ui tests

--- Tests iOS/MarsRoomUITesting.swift
+++ Tests iOS/MarsRoomUITesting.swift
@@ -8,34 +8,40 @@
 import XCTest
 
 final class MarsRoomUITesting: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app = XCUIApplication()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
+        let marsTab = app.tabBars["Tab Bar"].buttons["Mars Room"]
+        marsTab.tap()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAboutGoesBackToMarsRoom() throws {
+        
+        let aboutButton = app.buttons["About"]
+        let backMars = app.buttons["<Mars Room"]
+        
+        XCTAssertTrue(aboutButton.exists)
+        aboutButton.tap()
+        XCTAssertTrue(backMars.exists)
+        backMars.tap()
+        XCTAssertTrue(aboutButton.exists)
+        
+                        
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testBeforeAfterGoState() throws {
+        let goButton = app.buttons["Go"]
+        let introText = app.staticTexts["Pick a date and select go for an Evengeline Adams(ghostwriter Crowley) Sun Reading and sign Mars is in on this date."]
+        
+        XCTAssertTrue(introText.exists)
+        goButton.tap()
+        XCTAssertTrue(!introText.exists)
     }
 }
